b69064b5eb6018318b9b3a962f14c67a881887a5
sanghun <vitash1215@gmail.com>
2025-02-21 01:08:26 +0900

    fix : bean 중복 문제 해결
    String, String으로 key value를 사용하고 있는 상황이여서 StringRedisTemplate으로 변경

--- backend/src/main/java/com/techeer/backend/global/redis/RedisService.java
+++ backend/src/main/java/com/techeer/backend/global/redis/RedisService.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,22 +17,21 @@ public class RedisService {
     @Value("${jwt.refresh.expiration}")
     private Long refreshTokenExpirationPeriod;
 
-    private final RedisTemplate<String, String> redisTemplate;
+    private final StringRedisTemplate stringRedisTemplate;
 
     public String refreshTokenGet(String refreshToken) {
-        return redisTemplate.opsForValue().get("refreshToken:"+refreshToken);
+        return stringRedisTemplate.opsForValue().get("refreshToken:" + refreshToken);
     }
 
     public void cacheRefreshToken(String refreshToken) {
         String key = "refreshToken:" + refreshToken;
         log.info("refresh token cache key: {}", key);
-        // 리프레시 토큰을 Redis에 저장 (예: 7일 만료)
-        redisTemplate.opsForValue().set(key, refreshToken, Duration.ofMillis(refreshTokenExpirationPeriod));
+        stringRedisTemplate.opsForValue().set(key, refreshToken, Duration.ofMillis(refreshTokenExpirationPeriod));
     }
 
     public void deleteCacheRefreshToken(String refreshToken) {
         String key = "refreshToken:" + refreshToken;
         log.info("refresh token cache key: {}", key);
-        redisTemplate.delete(key);
+        stringRedisTemplate.delete(key);
     }
 }
