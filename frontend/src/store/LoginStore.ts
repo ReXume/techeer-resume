789be593d1a9cd480dbca55fbd19b436a471e381
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2025-02-22 05:08:15 +0900

    fix : LoginStore 수정

--- frontend/src/store/LoginStore.ts
+++ frontend/src/store/LoginStore.ts
@@ -1,5 +1,10 @@
 import { create } from "zustand";
 
+interface ResumeStore {
+  loginStatus: number;
+  setLoginStatus: (id: number) => void;
+}
+
 interface UserInfo {
   username: string;
   email: string;
@@ -10,6 +15,11 @@ interface UserInfoStore {
   userData: UserInfo | null; // userData를 객체로 정의 (초기값은 null)
   setUserData: (info: UserInfo) => void;
 }
+
+export const useLoginStatus = create<ResumeStore>((set) => ({
+  loginStatus: 0,
+  setLoginStatus: (id) => set({ loginStatus: id }),
+}));
 
 export const useUserInfo = create<UserInfoStore>((set) => ({
   userData: null,
