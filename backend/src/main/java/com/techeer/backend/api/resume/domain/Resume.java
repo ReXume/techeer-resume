ddbaf7ef265fa3c333997a3a284ac3a13510afc8
sanghun <vitash1215@gmail.com>
2025-02-23 02:44:28 +0900

    fix : view_count 추가
    view count 추가로 개별 조회 시 view가 증가, 조회 로직에서 output값으로 view_count 추가

--- backend/src/main/java/com/techeer/backend/api/resume/domain/Resume.java
+++ backend/src/main/java/com/techeer/backend/api/resume/domain/Resume.java
@@ -18,6 +18,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
@@ -60,7 +61,7 @@ public class Resume extends BaseEntity {
 
     // 조회수
     @Column(name = "view_count")
-    private Long viewCount = 0L;
+    private Long viewCount;
 
     @Builder.Default
     @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
@@ -81,6 +82,14 @@ public class Resume extends BaseEntity {
         this.career = career;
         this.position = position;
         this.resumePdf = resumePdf;
+        this.viewCount = 0L;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        if (this.viewCount == null) {
+            this.viewCount = 0L;
+        }
     }
 
     public void addResumeTechStack(ResumeTechStack resumeTechStack) {
