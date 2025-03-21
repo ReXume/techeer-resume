a9d926d937fd351f872a107de1c8d76103e9eb89
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2025-03-22 04:17:06 +0900

    chore : Sentry

---
+++ frontend/src/components/common/ErrorButton.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ErrorButton = () => {
+  return (
+    <button
+      onClick={() => {
+        throw new Error("This is your first error!");
+      }}
+    >
+      Break the world
+    </button>
+  );
+};
+
+export default ErrorButton;
