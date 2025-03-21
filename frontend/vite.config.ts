00011cb93d178b3451fae1c73d18ee3a6dc43c46
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2025-03-22 04:17:06 +0900

    chore : Sentry

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,14 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "rexume-7b",
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 });
