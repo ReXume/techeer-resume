4e958eda4a255cbc4fbcbbf420216bde9e2ec27c
sanghun <vitash1215@gmail.com>
2025-12-03 20:28:02 +0900

    UserConverter에 null 처리 전략 추가
    프로필 이미지가 null일 경우 명시적으로
    null로 매핑되도록 NullValuePropertyMappingStrategy를
    추가했습니다.
    
    이를 통해 프로필 이미지가 없는 사용자의 경우
    명확하게 null 값이 반환됩니다.

--- backend/src/main/java/com/techeer/backend/api/user/converter/UserConverter.java
+++ backend/src/main/java/com/techeer/backend/api/user/converter/UserConverter.java
@@ -4,6 +4,7 @@ import com.techeer.backend.api.user.domain.User;
 import com.techeer.backend.api.user.dto.response.UserInfoResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -12,7 +13,8 @@ public interface UserConverter {
 	UserConverter INSTANCE = Mappers.getMapper(UserConverter.class);
 
 	@Mapping(source = "id", target = "userId")
-	@Mapping(source = "profileImage.fileUrl", target = "profileImage")
+	@Mapping(source = "profileImage.fileUrl", target = "profileImage", 
+			nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
 	UserInfoResponse toUserInfoResponse(User user);
 
 }
