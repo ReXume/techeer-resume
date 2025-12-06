66300cf54399728ead255a18ffdaf1422f21716f
sanghun <vitash1215@gmail.com>
2025-12-07 02:53:11 +0900

    SignUpRequest를 Record로 변환 및 필드명 변경 (username -> name)

--- backend/src/main/java/com/techeer/backend/api/user/dto/request/SignUpRequest.java
+++ backend/src/main/java/com/techeer/backend/api/user/dto/request/SignUpRequest.java
@@ -3,15 +3,11 @@ package com.techeer.backend.api.user.dto.request;
 import com.techeer.backend.api.user.domain.Role;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
 
-@Getter
-public class SignUpRequest {
+public record SignUpRequest(
+    @NotBlank(message = "이름을 입력하세요")
+    String name,
 
-	@NotBlank(message = "이름 입력하세요")
-	private String username;
-
-	@NotNull(message = "권한을 입력하세요 (REGULAR, TECHEER, ADMIN)")
-	private Role role;
-
-}
+    @NotNull(message = "권한을 입력하세요 (REGULAR, TECHEER, ADMIN)")
+    Role role
+) {}
